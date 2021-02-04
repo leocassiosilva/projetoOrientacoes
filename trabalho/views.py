82bed1f5ec3488f152ecc2c88a11d122a3066089
leocassiosilva <leocassiosilva1234@gmail.com>
2021-02-04 07:20:17 -0300

    finalizando cadastro de trabalhos

--- trabalho/views.py
+++ trabalho/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import CreateView
 
 from trabalho.forms import TrabalhoModelForm
@@ -9,3 +10,12 @@ class TrabalhoCreate(CreateView):
     model = Trabalho
     form_class = TrabalhoModelForm
     template_name = 'trabalho/new-trabalho.html'
+
+    def form_valid(self, form):
+        trabalho = form.save(commit=False)
+        trabalho.id_usuario = self.request.user
+        trabalho.save()
+        return super(TrabalhoCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('index')
