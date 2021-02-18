fd7a6934e25730c685b883e9972359c09b983a48
leocassiosilva <leocassiosilva1234@gmail.com>
2021-02-17 22:49:48 -0300

    adicionando campos no template e pegando o trabalho na view

--- tarefa/forms.py
+++ tarefa/forms.py
@@ -6,4 +6,4 @@ from tarefa.models import Tarefa
 class TarefaModelForm(forms.ModelForm):
     class Meta:
         model = Tarefa
-        fields = ['nome', 'descricao', 'data_realizacao', 'tipo_tarefa', 'tipo_tarefa', 'trabalho']
+        fields = ['nome', 'descricao', 'data_realizacao', 'tipo_tarefa']
--- tarefa/views.py
+++ tarefa/views.py
@@ -6,13 +6,22 @@ from django.views.generic import CreateView
 
 from tarefa.forms import TarefaModelForm
 from tarefa.models import Tarefa
+from trabalho.models import Trabalho
 
 
 class TarefaView(CreateView):
     model = Tarefa
     form_class = TarefaModelForm
     template_name = 'tarefa/new-tarefa.html'
-    success_message = "Senha alterada com sucesso!"
+    success_message = "Tarefa cadastrada com sucesso!"
+
+    def form_valid(self, form):
+        tarefa = form.save(commit=False)
+        token = self.kwargs.get("token")
+        trabalho_id = Trabalho.objects.get(token=token)
+        tarefa.trabalho = trabalho_id
+        tarefa.save()
+        return super(TarefaView, self).form_valid(form)
 
     def get_success_url(self):
         return reverse('index')
