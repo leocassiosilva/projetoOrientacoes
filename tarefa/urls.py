b903fbb0c0b82ccba1c13858eb7afb7612b8b369
leocassiosilva <leocassiosilva1234@gmail.com>
2021-02-18 17:58:19 -0300

    finalizando a listagem de tarefas

--- tarefa/urls.py
+++ tarefa/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import TarefaView
+from .views import TarefaView, TarefaListView
 
 urlpatterns = [
     path('cadastrar/<str:token>', TarefaView.as_view(), name='tarefa_cadastrar'),
+    path('listar/<str:token>', TarefaListView.as_view(), name='tarefa_listar'),
 
 ]
--- tarefa/views.py
+++ tarefa/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from tarefa.forms import TarefaModelForm
 from tarefa.models import Tarefa
@@ -25,3 +25,14 @@ class TarefaView(CreateView):
 
     def get_success_url(self):
         return reverse('index')
+
+
+class TarefaListView(ListView):
+    model = Tarefa
+    template_name = 'tarefa/tarefa_listar.html'
+
+    def get_queryset(self):
+        token = self.kwargs.get("token")
+        trabalho_id = list(trabalho.id for trabalho in Trabalho.objects.filter(token=token))
+        tarefa = Tarefa.objects.filter(trabalho__in=trabalho_id)
+        return tarefa
