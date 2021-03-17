5614ec58c542bc4ba05f7098f445d184929a903d
leocassiosilva <leocassiosilva1234@gmail.com>
2021-03-17 19:07:35 -0300

    io

--- projetoOrientacoes/settings.py
+++ projetoOrientacoes/settings.py
@@ -166,7 +166,6 @@ CELERY_TIMEZONE = 'America/Sao_Paulo'
 django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
